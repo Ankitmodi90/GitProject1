63b1d3f50d49a01bd2d8cf03d2fa97da275ad447
Ankitmodi90 <Ankitmodi90@outlook.com>
2023-05-13 15:33:29 +0530

    First Commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+import java.nio.file.Paths;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.webkit().launch();
+            Page page = browser.newPage();
+            page.navigate("http://whatsmyuseragent.org/");
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+        }
+    }
+}
